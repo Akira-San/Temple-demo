656a37437cc6da7521a8eddbb829efe939ae3ad2
Akira-San <38386020+Akira-San@users.noreply.github.com>
2018-04-16 06:11:02 +0800

    Add files via upload

---
+++ Hero.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Hero : MonoBehaviour {
+
+    public NavMeshAgent agent;
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                //print(hit.point);
+                agent.SetDestination(hit.point);
+            }
+        }
+        anim.SetFloat("speed", agent.velocity.magnitude);
+	}
+}
